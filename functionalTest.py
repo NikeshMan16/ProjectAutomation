68743c1a33efe46a451c162477ff6849f111852b
NikeshMan16 <nikeshmanshrestha@gmail.com>
2025-03-24 23:25:55 +0545

    functionalTest

--- functionalTest.py
+++ functionalTest.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 from selenium import webdriver
 from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
@@ -7,8 +6,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
 from utils.utils import take_screenshot
+import random
+import pytest
 
 os.makedirs("screenshots", exist_ok=True)
 
@@ -26,6 +26,13 @@ def login_function(setup):
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID,'password'))).send_keys("secret_sauce")
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID,'login-button'))).click()
     time.sleep(2)
+
+def reset_app_state(setup): # Only works when a user is logged in
+    driver = setup
+    time.sleep(1)
+    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.ID,'react-burger-menu-btn'))).click()
+    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.ID,'reset_sidebar_link'))).click()
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID,'react-burger-cross-btn'))).click()
 
 def test_navigation_to_cart(setup):
     driver = setup
@@ -55,27 +62,64 @@ def test_navigation_from_cart_to_inventory(setup):
 def test_add_to_cart_items_count(setup):
     driver = setup
     driver.get("https://www.saucedemo.com/inventory.html")
+    driver.maximize_window()
     time.sleep(2)
     # Find all "Add to cart" buttons
     add_to_cart_buttons = driver.find_elements(By.XPATH, "//button[contains(text(),'Add to cart')]")
     expected_count = len(add_to_cart_buttons)  # Get the number of available products
     for button in add_to_cart_buttons:   # Click all "Add to Cart" buttons
         button.click()
-        time.sleep(1)  # Ensure UI updates
+        time.sleep(1.5)  # Ensure UI updates
     cart_badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text # Retrieve and validate cart count
     actual_count = int(cart_badge)
+    time.sleep(1)
+    # driver.find_element(By.ID,'shopping_cart_container').click()
+    # time.sleep(3)
     try:
         assert actual_count == expected_count, f"Expected {expected_count} items in cart, but found {actual_count}"
     except AssertionError as e:
         take_screenshot(driver)
         pytest.fail(str(e))
 
-#
-# def test_verify_added_cart_items(setup):
-#     driver = setup
-#     driver.get("https://www.saucedemo.com/inventory.html")
-#     time.sleep(2)
-#
+
+
+def test_verify_added_cart_items(setup):
+    driver = setup
+    driver.get("https://www.saucedemo.com/inventory.html")
+    time.sleep(2)
+    reset_app_state(setup)
+    time.sleep(2)
+    # Finding all the inventory items and their names
+    inventory_items = driver.find_elements(By.CLASS_NAME,'inventory_item')
+    add_to_cart_buttons = driver.find_elements(By.XPATH,'//button[contains(text(),"Add to Cart")]')
+
+    #Select random items to add to Cart
+    selected_items = random.sample(list(zip(inventory_items,add_to_cart_buttons)),k=3)
+    added_items = []
+
+    for item,button in selected_items:
+        item_name = item.find_element(By.CLASS_NAME,'inventory_item_name').text
+        added_items.append(item_name)
+        button.click()
+        time.sleep(1)  #Wait for the UI update
+
+    # Navigate to Cart
+    driver.find_element(By.ID,'shopping_cart_container').click()
+    time.sleep(2)
+
+    #Verify add items are displayed in the cart
+    cart_items = driver.find_elements(By.CLASS_NAME,'inventory_item_name')
+    cart_items_name = [item.text for item in cart_items]
+
+    try:
+        assert set(added_items) == set(cart_items_name) , f"Mismatch in cart items, Expected items:{added_items}, but found {cart_items_name}"
+    except AssertionError as e:
+        take_screenshot(driver)
+        pytest.fail(str(e))
+
+
+
+
 
 
 
